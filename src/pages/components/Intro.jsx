8b4698fcb2a8258d8f1bde5a5dfc5c26d352de70
Faizaan Vidhani <39887209+faizaanvidhani@users.noreply.github.com>
2022-12-26 16:21:27 -0600

    Update Intro.jsx

--- src/pages/components/Intro.jsx
+++ src/pages/components/Intro.jsx
@@ -4,7 +4,7 @@ export default function Intro() {
     return (
         <div className="intro">
             <p className="intro-heading">
-                Hi, I'm EcstaticGoat777. I'm a Software Engineer with a BS in Neuroscience
+                Hi! I'm Faizaan Vidhani. I'm a Software Engineer with a BS in Neuroscience
                 and Computer Science from <a class="intro-brown-link" href="https://www.brown.edu/">Brown University</a>.
             </p>
             <p className="intro-body">
